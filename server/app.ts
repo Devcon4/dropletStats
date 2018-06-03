bc384f4adfc7a19d8f0489a981334829c4faca64
Devcon4 <devcon42@gmail.com>
2018-06-03 15:31:55 -0600

    made some changes

--- server/app.ts
+++ server/app.ts
@@ -5,42 +5,66 @@ let ps = procfs(9850);
 
 let app = express();
 
+let logging = (req) => {
+    console.log(`request: {req.path}`);    
+}
+
+let error = (err) => {
+    if(err) {
+        console.error(err);
+    }
+}
+
 app.get('/api/io', (req, res) => {
-    ps.status(r => {
+    logging(req);
+    ps.io((err, r) => {
+        error(err);
         res.send(r);
     });
 });
 
 app.get('/api/stat', (req, res) => {
-    ps.status(r => {
+    logging(req);
+    ps.stat((err, r) => {
+        error(err);
         res.send(r);
     });
 });
 
 app.get('/api/statm', (req, res) => {
-    ps.status(r => {
+    logging(req);
+    ps.statm((err, r) => {
+        error(err);
         res.send(r);
     });
 });
 
 app.get('/api/status', (req, res) => {
-    ps.status(r => {
+    logging(req);
+    ps.status((err, r) => {
+        error(err);
         res.send(r);
     });
 });
 
 app.get('/api/cpu', (req, res) => {
-    ps.status(r => {
+    logging(req);
+    ps.cpu((err, r) => {
+        error(err);
         res.send(r);
     });
 });
 
 app.get('/api/meminfo', (req, res) => {
-    ps.status(r => {
+    logging(req);
+    ps.meminfo((err, r) => {
+        error(err);
         res.send(r);
     });
 });
 
 app.use(express.static(__dirname + '/public'));
+
+console.log('Express starting!');
 
 app.listen(80);
